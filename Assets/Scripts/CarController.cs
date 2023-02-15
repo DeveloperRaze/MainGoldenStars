d46154e72cff9afcaa5db750b225a6e34f14931e
UOC\ddavies6 <ddavies6@stu.chi.ac.uk>
2023-02-15 12:41:35 +0000

    Updated assets

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -15,58 +15,96 @@ public class CarController : MonoBehaviour
     private float currentbreakForce;
     private bool isBreaking;
 
+    public ControlMode control;
+
+    //Add Rigidbody to Vehicle.
     [SerializeField] Rigidbody RB;
 
-
+    //Serialise engine power, break force, and max steer angle. 
     [SerializeField] private float motorForce;
     [SerializeField] private float breakForce;
     [SerializeField] private float maxSteerAngle;
 
-    [SerializeField] float maxSpeed = 7.0f;
+    //Init speed at 6.
+    [SerializeField] float maxSpeed = 6.0f;
 
+    //Add reference to 4 wheel colliders
     [SerializeField] private WheelCollider frontLeftWheelCollider;
     [SerializeField] private WheelCollider frontRightWheelCollider;
     [SerializeField] private WheelCollider backLeftWheelCollider;
     [SerializeField] private WheelCollider backRightWheelCollider;
 
+    //Add reference to 4 wheel transforms
     [SerializeField] private Transform frontLeftWheelTransform;
     [SerializeField] private Transform frontRightWheelTransform;
     [SerializeField] private Transform backLeftWheelTransform;
     [SerializeField] private Transform backRightWheelTransform;
 
+    //Method to change input type(Keyboard will be removed as this is a mobile game)
+    public enum ControlMode
+    {
+        Keyboard,
+        Buttons
+    };
 
     // Update is called once per frame
-    private void  FixedUpdate()
+    private void Update()
     {
+        //Get input controls and update wheel transforms
         GetInput();
-        HandleMotor();
-        HandleSteering();
         UpdateWheels();
-
+        //Rigidbody velocity clamp
         RB.velocity = Vector3.ClampMagnitude(RB.velocity, maxSpeed);
     }
 
-    private void GetInput()
+    void LateUpdate() 
     {
-        horizontalInput = Input.GetAxis(HORIZONTAL);
-        verticalInput = Input.GetAxis(VERTICAL);
-        //isBreaking = Input.GetKey(KeyCode.Space);
+        //Handle engine and steering
+        HandleMotor();
+        HandleSteering();
+    }
+
+
+    public void MoveInput(float input)
+    {
+        //Handle forward/backward input
+        verticalInput = input;
+    }
+
+    public void SteerInput(float input)
+    {
+        //Handle forward/backward input
+        horizontalInput = input;
+    }
+
+    void GetInput()
+    {
+        //if input is keyboard
+        if (control == ControlMode.Keyboard)
+        {
+            verticalInput = Input.GetAxis(VERTICAL);
+            horizontalInput = Input.GetAxis(HORIZONTAL);
+            //isBreaking = Input.GetKey(KeyCode.Space);
+        }
     }
 
     private void HandleMotor()
     {
+        //Handle engine torque / velocity and breaking
         frontLeftWheelCollider.motorTorque = verticalInput * motorForce;
         frontRightWheelCollider.motorTorque = verticalInput * motorForce;
         currentbreakForce = isBreaking ? breakForce : 1f;
 
         if (isBreaking)
         {
+            //Unable to get this working. 
             ApplyBreaking();
         }
     }
 
     private void ApplyBreaking()
     {
+        //Unable to get this working. 
         frontLeftWheelCollider.brakeTorque = currentbreakForce;
         frontRightWheelCollider.brakeTorque = currentbreakForce;
         backLeftWheelCollider.brakeTorque = currentbreakForce;
@@ -75,6 +113,7 @@ public class CarController : MonoBehaviour
 
     private void HandleSteering()
     {
+        //Handle steering based on horizontal input. 
         currentSteerAngle = maxSteerAngle * horizontalInput;
         frontLeftWheelCollider.steerAngle = currentSteerAngle;
         frontRightWheelCollider.steerAngle = currentSteerAngle;
