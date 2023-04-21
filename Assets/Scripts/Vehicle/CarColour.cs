f210c541d80ec76238a2e5bcb289c5094006d207
Dom <87981413+DeveloperRaze@users.noreply.github.com>
2023-04-21 14:54:34 +0100

    Updates include adding a vehicle colour change button, animation for numbers and lighting in levels 2 3 and 4 fixed.

---
+++ Assets/Scripts/Vehicle/CarColour.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarColour : MonoBehaviour
+{
+
+    //Set these Textures in the Inspector
+
+    public Texture m_MainTexture;
+    public Texture m_RedTexture;
+    public Texture m_GreenTexture;
+
+    public Renderer rend;
+
+    public void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    public void SetMaterialMain()
+    {
+        rend.material.SetTexture ("_MainTex", m_MainTexture);
+    }
+
+    public void SetMaterialRed()
+    {
+        rend.material.SetTexture("_MainTex", m_RedTexture);
+    }
+
+    public void SetMaterialGreen()
+    {
+        rend.material.SetTexture("_MainTex", m_GreenTexture);
+    }
+
+}
